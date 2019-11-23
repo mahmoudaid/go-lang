6b89e22f06ff2955162f293ddc4e6862e085b671
Mahmoud Zayed <mahmoudeid2050@gmail.com>
2019-11-23 19:13:01 +0200

    Add the Largest sum of conteguous sub array

---
+++ 02-algorithms/01-array/05-largest-sum-contiguous-sub-array/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func maxSubArray(data []int) int {
+	size := len(data)
+	maxSoFar := 0
+	maxEndingHere := 0
+
+	for i := 0; i < size; i++ {
+		maxEndingHere += data[i]
+		if maxEndingHere < 0 {
+			maxEndingHere = 0
+		}
+
+		if maxSoFar < maxEndingHere {
+			maxSoFar = maxEndingHere
+		}
+	}
+
+	return maxSoFar
+}
+
+func main() {
+	data := []int{1, -2, 3, 4, -4, 6, -14, 6, 2}
+	fmt.Println("Max sub array sum", maxSubArray(data))
+}
